ee288df38257f4e5c9e4cbae7f6f8fd846399d76
Mateusz <umateusz@op.pl>
2019-07-08 23:40:06 +0200

    add load local files

--- dotdotdot.sh
+++ dotdotdot.sh
@@ -3,4 +3,8 @@ DOTDOTDOT_PATH=~/dotdotdot
 . $DOTDOTDOT_PATH/system/functions.sh
 . $DOTDOTDOT_PATH/system/aliases.sh
 
+for f in $DOTDOTDOT_PATH/local/*; do
+    . $f
+done
+
 eval $($DOTDOTDOT_PATH/system/functions.py)
