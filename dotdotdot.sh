ceb20e08c0f97624e3ef8cadb6dba26f0c748e1b
umateusz <umateusz@op.pl>
2021-08-30 23:02:42 +0200

    add bin dir

--- dotdotdot.sh
+++ dotdotdot.sh
@@ -2,6 +2,7 @@ DOTDOTDOT_PATH=~/dotdotdot
 
 . $DOTDOTDOT_PATH/system/functions.sh
 . $DOTDOTDOT_PATH/system/aliases.sh
+export PATH=$PATH:$DOTDOTDOT_PATH/system/bin
 
 for f in $DOTDOTDOT_PATH/local/*(.N); do
     . $f
