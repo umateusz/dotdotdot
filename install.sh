e2cb02fa0235e677595b192511714f96aed17b8b
umateusz <umateusz@op.pl>
2021-10-04 17:47:14 +0200

    set max_user_watches

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 echo "Running apt install..."
 sudo apt update -y && sudo apt install git ansible
 
+echo "Install ansible plugins..."
+ansible-galaxy collection install ansible.posix
+
 echo "Running ansible..."
 ansible-playbook --ask-become-pass -i ansible/inventory/local_host ansible/local.yml --extra-vars '{"dotdotdot_path":"~/dotdotdot"}'
